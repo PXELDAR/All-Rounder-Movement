57ed42eb7946b4ece04c7e9bc81046f241a26776
Serdar Al <serdara@playtika.com>
2022-09-20 14:55:35 +0300

    Added slope recognition

--- src/All-Rounder-Movement/Assets/Scripts/PhysicsSphere.cs
+++ src/All-Rounder-Movement/Assets/Scripts/PhysicsSphere.cs
@@ -8,22 +8,35 @@ namespace PXELDAR
 
         [SerializeField][Range(0f, 100f)] private float _maxSpeed = 5f;
         [SerializeField, Range(0f, 10f)] private float _jumpHeight = 2f;
-        [SerializeField, Range(0, 5)] private int _maxAirJumps = 0;
         [SerializeField, Range(0f, 100f)] private float _maxAcceleration = 10f;
         [SerializeField, Range(0f, 100f)] private float _maxAirAcceleration = 10f;
+        [SerializeField, Range(0f, 90f)] private float _maxGroundAngle = 25f;
+        [SerializeField, Range(0, 5)] private int _maxAirJumps = 0;
 
         private int _jumpPhase;
+        private int _groundContactCount;
+        private float _minGroundDotProduct;
+        private bool _desiredJump;
+        private bool _onGround => _groundContactCount > 0;
         private Vector3 _velocity;
         private Vector3 _desiredVelocity;
+        private Vector3 _contactNormal;
         private Rigidbody _rigidBody;
-        private bool _desiredJump;
-        private bool _onGround;
+
 
         //=======================================================================================
 
         private void Awake()
         {
             _rigidBody = GetComponent<Rigidbody>();
+            OnValidate();
+        }
+
+        //=======================================================================================
+
+        void OnValidate()
+        {
+            _minGroundDotProduct = Mathf.Cos(_maxGroundAngle * Mathf.Deg2Rad);
         }
 
         //=======================================================================================
@@ -46,11 +59,7 @@ namespace PXELDAR
         private void FixedUpdate()
         {
             UpdateState();
-
-            float acceleration = _onGround ? _maxAcceleration : _maxAirAcceleration;
-            float maxSpeedChange = acceleration * Time.deltaTime;
-            _velocity.x = Mathf.MoveTowards(_velocity.x, _desiredVelocity.x, maxSpeedChange);
-            _velocity.z = Mathf.MoveTowards(_velocity.z, _desiredVelocity.z, maxSpeedChange);
+            AdjustVelocity();
 
             if (_desiredJump)
             {
@@ -59,7 +68,7 @@ namespace PXELDAR
             }
 
             _rigidBody.velocity = _velocity;
-            _onGround = false;
+            ClearState();
         }
 
         //=======================================================================================
@@ -71,6 +80,14 @@ namespace PXELDAR
             if (_onGround)
             {
                 _jumpPhase = 0;
+                if (_groundContactCount > 1)
+                {
+                    _contactNormal.Normalize();
+                }
+            }
+            else
+            {
+                _contactNormal = Vector3.up;
             }
         }
 
@@ -82,13 +99,14 @@ namespace PXELDAR
             {
                 _jumpPhase++;
                 float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * _jumpHeight);
+                float alignedSpeed = Vector3.Dot(_velocity, _contactNormal);
 
-                if (_velocity.y > 0f)
+                if (alignedSpeed > 0f)
                 {
-                    jumpSpeed = Mathf.Max(jumpSpeed - _velocity.y, 0f);
+                    jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
                 }
 
-                _velocity.y += jumpSpeed;
+                _velocity += _contactNormal * jumpSpeed;
             }
         }
 
@@ -114,8 +132,52 @@ namespace PXELDAR
             for (int i = 0; i < collision.contactCount; i++)
             {
                 Vector3 normal = collision.GetContact(i).normal;
-                _onGround |= normal.y > 0.9f;
+                if (normal.y >= _minGroundDotProduct)
+                {
+                    _groundContactCount++;
+                    _contactNormal += normal;
+                }
+                else
+                {
+                    _contactNormal = Vector3.up;
+                }
             }
+        }
+
+        //=======================================================================================
+
+        void AdjustVelocity()
+        {
+            Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
+            Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+
+            float currentX = Vector3.Dot(_velocity, xAxis);
+            float currentZ = Vector3.Dot(_velocity, zAxis);
+
+            float acceleration = _onGround ? _maxAcceleration : _maxAirAcceleration;
+            float maxSpeedChange = acceleration * Time.deltaTime;
+
+            float newX =
+                Mathf.MoveTowards(currentX, _desiredVelocity.x, maxSpeedChange);
+            float newZ =
+                Mathf.MoveTowards(currentZ, _desiredVelocity.z, maxSpeedChange);
+
+            _velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+        }
+
+        //=======================================================================================
+
+        private Vector3 ProjectOnContactPlane(Vector3 vector)
+        {
+            return vector - _contactNormal * Vector3.Dot(vector, _contactNormal);
+        }
+
+        //=======================================================================================
+
+        private void ClearState()
+        {
+            _groundContactCount = 0;
+            _contactNormal = Vector3.zero;
         }
 
         //=======================================================================================
